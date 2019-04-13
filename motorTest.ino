69c118181c2f8f7a00b3a0c58deafed2ecd1069d
estrada9707 <43385179+estrada9707@users.noreply.github.com>
2019-04-13 15:48:53 -0500

    Add files via upload

--- motorTest.ino
+++ motorTest.ino
@@ -1,54 +1,151 @@
 void setup() 
 {
-  pinMode(22, OUTPUT);
+  pinMode(2, OUTPUT);
   // put your setup code here, to run once:
 
 }
 
 void loop() 
 {
+analogWrite(13, 255);
 
 
-for (int i=0; i <= 255; i++)
+
+
+
+
+/*for (int i=0; i <= 255; i++)
 {
-      analogWrite(8, i);
-      delay(10);
+      analogWrite(13, i);
+      //delay(5000);
  }
+delay(5000);
+
+
+for (int i=255; i >= 255; i--)
+{
+      analogWrite(13, i);
+      //delay(5000);
+ }
+delay(5000);
+*/
+/*
+delay(500);
+digitalWrite(1, HIGH);
+analogWrite(13, 255);
+delay(500);
+analogWrite(13, 200);
+delay(500);
+analogWrite(13, 150);
+delay(500);
+analogWrite(13, 100);
+delay(500);
+analogWrite(13, 50);
+delay(500);
+analogWrite(13, 0);
+delay(500);
+*/
+
+ /*
+delay(1000);
+
+digitalWrite(1, LOW);
+for(int i=0; i<=255; i++)
+{
+  analogWrite(13, i);
+  delay(10);
+}
+/////////
+*/
+
 
 
 /*
-delay(1000);
-digitalWrite(22, HIGH);
-analogWrite(8, 255);
-delay(500);
-analogWrite(8, 200);
-delay(500);
-analogWrite(8, 150);
-delay(500);
-analogWrite(8, 100);
-delay(500);
-analogWrite(8, 50);
-delay(500);
-analogWrite(8, 0);
-delay(500);
+digitalWrite(1, HIGH);
+for(int i=255; i>=0; --i)
+{
+  analogWrite(13, i);
+  delay(10);
+}
+
+delay(2000);
 
 
-digitalWrite(22, LOW);
-analogWrite(8, 50);
-delay(500);
-analogWrite(8, 100);
-delay(500);
-analogWrite(8, 150);
-delay(500);
-analogWrite(8, 200);
-delay(500);
-analogWrite(8, 255);
-delay(1000);
-//analogWrite(8, 0);
-//delay(1000);
+digitalWrite(1, LOW);
+for(int i=0; i<=0; i++)
+{
+  analogWrite(13, i);
+  delay(10);
+}
 
- 
+delay(3000);
+
+/*
+digitalWrite(1, LOW);
+analogWrite(13, 0);
+delay(500);
+analogWrite(13, 50);
+delay(500);
+analogWrite(13, 100);
+delay(500);
+analogWrite(13, 150);
+delay(500);
+analogWrite(13, 200);
+delay(500);
+analogWrite(13,255);
+delay(3000);
 */
+
+/*
+digitalWrite(1, HIGH);
+for (int i = 255; i >= 0; --i) 
+{
+  analogWrite(13, i);
+  delay(500);
+}
+ delay(2500);
+ digitalWrite(1, LOW);
+
+ for (int i= 0; i <=0; i++) 
+{
+  analogWrite(13, i);
+  delay(500);
+}
+ delay(2500);
+*/
+
+/*
+//delay(1000);
+digitalWrite(1, LOW);
+
+for (int i=0; i <= 255; i++)
+{
+  
+      analogWrite(13, i);
+      i=i+50;
+    
+ }
+delay(2500);
+
+*/
+
+
+/*digitalWrite(1, LOW);
+analogWrite(13, 50);
+delay(500);
+analogWrite(13, 100);
+delay(500);
+analogWrite(13, 150);
+delay(500);
+analogWrite(13, 200);
+delay(500);
+analogWrite(13, 255);
+delay(10000);
+//analogWrite(13, 0);
+//delay(1000);
+*/
+ 
+
   
   // put your main code here, to run repeatedly:
 
